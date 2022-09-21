df69b485b919674b30f4328e7e1ad96848777e4a
Eren Tasci <erent4sci@gmail.com>
2022-09-21 16:02:55 +0300

    Added 06-menu

---
+++ 06-menu/src/components/Categories.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Categories = ({categories,filterItems}) => {
+  return (
+  <div className='btn-container'>
+
+    {
+      categories.map( (category,index) => {
+        return (
+          <button
+            type='button'
+            className='filter-btn'
+            onClick={()=>
+              {filterItems(category) 
+              console.log(category)}}
+            key={index}
+          >
+            {category}
+          </button> 
+        ) 
+      } )
+    }
+
+  </div>);
+};
+
+export default Categories;
